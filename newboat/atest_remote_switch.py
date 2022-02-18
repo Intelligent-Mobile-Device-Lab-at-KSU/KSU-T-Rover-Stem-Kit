28483d5713d013e91d677e64a83764d32955489d
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-18 14:23:41 -0500

    Update atest_remote_switch.py

--- newboat/atest_remote_switch.py
+++ newboat/atest_remote_switch.py
@@ -327,7 +327,7 @@ def main():
     print('KSU-Boat Initializing...')
     print(' ')
     print('Setting Up Remote Web Switch (check for manual/autonomous)')
-    th_updateDriveMode = threading.Thread(name='fetchDriveMode', target=fetchDriveMode, args=())
+    th_updateDriveMode = threading.Thread(name='fetchDriveMode', target=fetchDriveMode)
     th_updateDriveMode.start()
     ############START TCP GPS Client Init###################
     print('Setting Up Termux TCP Client for incoming GPS...')
